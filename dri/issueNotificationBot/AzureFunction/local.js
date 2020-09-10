de9d7edb5e377baf8712dead4008760030a695bf
Michael Richardson <v-micric@microsoft.com>
2020-09-10 14:08:25 -0700

    remove useless console binding

--- dri/issueNotificationBot/AzureFunction/local.js
+++ dri/issueNotificationBot/AzureFunction/local.js
@@ -5,9 +5,4 @@ const ENV_FILE = path.join(__dirname, '.env');
 require('dotenv').config({ path: ENV_FILE });
 process.env.BotBaseUrl = 'http://localhost:3978';
 
-// The Azure function expects a `context` object, which contains the
-// names of all of the bindings in function.json. To process PRs,
-// we need to ensure the atEightAm binding exists on console.
-console.bindings = { atEightAM: true };
-
 runTracker(console);
